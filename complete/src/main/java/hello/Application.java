581616f4b19a722ddb4f48302b857516b4d33791
Katherine Guzman <katherineguzman@Katherines-MacBook.local>
2019-01-14 11:18:25 -0500

    First commit

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -3,10 +3,14 @@ package hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // this is a convenience annotation that adds all of the following:
+/*
+* @Configuration tags the class as a source of bean definitions for the application context
+* @EnableAutoConfiguration tells Spring Boot to start adding beans based on classpath settings,
+* other beans, and various property settings. **/
 public class Application {
-
     public static void main(String[] args) {
+        /* The main() method uses Spring Boot's Spring*/
         SpringApplication.run(Application.class, args);
     }
 }
--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -1,19 +1,20 @@
 package hello;
 
-import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+    @GetMapping("/greeting") // ensures HTTP GET requests to /greeting are mapped to greeting() method
+// @RequestParam binds the value of the query String parameter "name" into the "name" parameter of the greeting() method
+// NOTE: The query string paramter "name" isn't required; if it's absent, the defaultValue of "World" is used
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
+
 }
+
